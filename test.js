2ca4d1acc6578af7ecb77e85754326e37ba90889
Moiz <progrmoiz@gmail.com>
2017-12-07 21:21:47 +0500

    added test

--- test.js
+++ test.js
@@ -1,6 +1,18 @@
-import test from 'ava'
-import execa from 'execa'
+import test from 'ava';
+import execa from 'execa';
 
-test(async t => {
-	t.is(await execa('./cli.js', ['freeCodeCamp']), /* testing */)
+test('connectivity test', async t => {
+	t.truthy(await execa('./cli.js', ['progrmoiz']).stdout);
+});
+
+test('sorting test', async t => {
+	const cmd = await execa('./cli.js', ['freeCodeCamp']);
+	const firstLine = cmd.stdout.split('\n')[0];
+	t.true(firstLine.includes('freeCodeCamp'));
+});
+
+test('order test', async t => {
+	const cmd = await execa('./cli.js', ['freeCodeCamp', '--order=ASC']);
+	const lastLine = cmd.stdout.split('\n').pop();
+	t.true(lastLine.includes('freeCodeCamp'));
 });
